d76e52e241f1813a16470b601bc9f22167c46021
nishantxrana <nishantrana249000@gmail.com>
2024-04-06 16:27:02 +0530

    Project Setup

---
+++ client/src/App.jsx
@@ -0,0 +1,10 @@
+ function App() {
+ 
+  return (
+    <>
+    <div className=' flex w-full h-screen justify-center items-center'><h1 className=' text-3xl text-red-500'>The app is working fine</h1></div>
+    </>
+  )
+}
+
+export default App
