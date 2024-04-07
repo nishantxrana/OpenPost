8e6eaa082cfc2ca0c246fab9e175be80d4b97ce8
nishantxrana <nishantrana249000@gmail.com>
2024-04-07 23:20:44 +0530

    sign in backend

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import { errorDisplay } from "../../utils/error.js";
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
+import jwt from 'jsonwebtoken'
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = await req.body;
@@ -26,13 +27,13 @@ export const signup = async (req, res, next) => {
   // }
 
   //simple hasshing
-  // const hashedPassword = await bcryptjs.hash(password, 10);
+  const hashedPassword = await bcryptjs.hash(password, 10);
 
   // creating new user
   const newUser = new User({
     username,
     email,
-    password
+    password:hashedPassword,
   });
 
   // checking if there is an error
@@ -43,3 +44,36 @@ export const signup = async (req, res, next) => {
     next(error);
   }
 };
+
+export const signin = async(req, res, next)=> {
+  const { email, password } = await req.body;
+
+  // checking if any of the feild is empty
+  if (!email ||!password || email === "" || password === "") {
+    next(errorDisplay(400, "fill all fields"));
+  }
+  //checking if user already exists
+
+  try {
+    const existingUser = await User.findOne({email});
+    if(!existingUser) {
+      return res.status(400).json({success:false,message: "User does not exist"});
+    }
+    // checking if password is correct
+    
+    const passCheck = await bcryptjs.compare(password, existingUser.password);
+    if(!passCheck ){
+      return res.status(400).json({success:false,message: "Password is incorrect"});
+    }
+
+    //spreading the existing user to remove password from it
+    const {password:pas,...rest} = existingUser._doc
+    // if password is correct send authentication token
+    const token = jwt.sign({id : existingUser._id}, process.env.Token_key,{expiresIn:'1d'});
+    res.status(200).cookie('login_token',token,{httpOnly:true}).json({rest}); //TODO: in this we need to send cookie first 
+    
+    
+  } catch (error) {
+    next(error)
+  }
+}
--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,8 +1,9 @@
 import express from 'express';
-import { signup } from '../controllers/auth.controller.js';
+import { signin, signup } from '../controllers/auth.controller.js';
 
 const auth = express.Router();
 
 auth.post('/signup',signup)
+auth.post('/signin',signin)
 
 export default auth
