24e5e1f2208895a637380ab34c239f12a381184e
nishantxrana <nishantrana249000@gmail.com>
2024-04-26 17:30:30 +0530

    deployment changes

--- api/index.js
+++ api/index.js
@@ -6,6 +6,7 @@ import auth from './routes/auth.route.js';
 import cookieParser from 'cookie-parser'
 import postRoute from './routes/post.route.js';
 import commentRouter from './routes/comment.route.js';
+import path from 'path'
 // import { env } from 'process'; // can use this to env.mongoKey
 
 dotenv.config()
@@ -18,6 +19,8 @@ mongoose.connect(process.env.mongoKey).then(()=>{
 });
 
 let port = 3000;
+const __dirname = path.resolve();
+
 const app = express()
 app.use(express.json());
 app.use(cookieParser()); //FIXME: always define middleware before using it (before using routes)
@@ -29,13 +32,17 @@ app.use('/api/auth',auth)
 app.use('/api/post',postRoute)
 app.use('/api/comment',commentRouter)
 
+app.use(express.static(path.join(__dirname, 'client/dist')));
+app.use('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+  })
+
+
 app.listen(port,()=>{
     console.log(`listening on ${port}`);
 })
 
-app.get('/cookie',(req, res )=>{
-    res.json(req.cookies.login_token)
-})
+
 
 app.use((err,req,res,next)=>{
     const message = err.message || 'internal error';
